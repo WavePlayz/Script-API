d1d394b0b136fab522cccb85e62a79dd747db73a
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2021-08-04 16:23:46 +0530

    Update script.js

--- sample_pack/scripts/script.js
+++ sample_pack/scripts/script.js
@@ -1,1 +1,9 @@
+import { World, Commands } from "Minecraft"
 
+let tickCount = 0
+
+function onTick(eventData) {
+	Commands.run("title @a actionbar " + tickCount++)
+}
+
+World.events.tick.subscribe(onTick)
