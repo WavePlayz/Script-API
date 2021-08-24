1ffec75e350255ff674a1a30d51aac59591b9c9d
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2021-08-24 20:40:20 +0530

    Delete sample_pack directory

--- sample_pack/scripts/script.js
+++
@@ -1,9 +0,0 @@
-import { World, Commands } from "Minecraft"
-
-let tickCount = 0
-
-function onTick(eventData) {
-	Commands.run("title @a actionbar " + tickCount++)
-}
-
-World.events.tick.subscribe(onTick)
