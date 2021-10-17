73976c561f4500d435423d35449c52de3fa574d2
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2021-10-17 14:43:20 +0530

    update chat-detection.js

--- examples/chat-detection.js
+++ examples/chat-detection.js
@@ -24,4 +24,28 @@ World.events.chat.subscribe(eventData => {
 	}
 })
 
+const N_WORDS = [ "fuq", "hi", "hello" ]
+
+World.events.beforeChat.subscribe(eventData => {
+	const { entity, message } = eventData
+	
+	let containsNWord = N_WORDS.some(word => message.includes(word))
+	
+	if (containsNWord) {
+		let entityName = entity.name ?? entity.nameTag ?? "yoo"
+		
+		displayChat(`§e${entityName}! found a n-word mate, ima hide it.`, OVERWORLD)
+		
+		eventData.cancel = true
+		
+		let cleanedMessage
+		
+		N_WORDS.forEach(word => {
+			cleanedMessage = message.replaceAll(word, "*beep*")
+		})
+		
+		displayChat(`${entityName}! found n-word mate, ima hide it.`, OVERWORLD)
+	}
+})
+
 
