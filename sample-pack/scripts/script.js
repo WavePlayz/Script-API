f1c40d2bd85c87ebf4ccdffd0463c9397e75d48f
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2021-10-30 18:35:38 +0530

    Update script.js

--- sample-pack/scripts/script.js
+++ sample-pack/scripts/script.js
@@ -2,6 +2,10 @@
 import * as Minecraft from "mojang-minecraft";
 import * as Gametest from "mojang-gametest";
 
+const OVERWORLD = Minecraft.World.getDimension("overworld");
+
+Minecraft.Commands.run("say Hello World", OVERWORLD)
+
 /**
 * Your code here
 **/
