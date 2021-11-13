d897d2c9068d56a057b451cdcd8d3a9a5e92a394
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2021-11-13 10:40:23 +0530

    Update vector3.js

--- libraries/vector3.js
+++ libraries/vector3.js
@@ -1,1 +1,42 @@
 
+class Vector3 {
+	static RADIANS_IN_DEGREE = Math.PI / 180
+	static DEGREES_IN_RADIAN = 180 / Math.PI
+	
+	static degreeToRadians (degrees) {
+		return degrees * this.RADIANS_IN_DEGREE
+	}
+	
+	static radianToDegrees (radians) {
+		return radians * this.DEGREES_IN_RADIAN
+	}
+	
+	static fromAngle (pitch, yaw) {
+		let pitchRad = this.degreeToRadians(pitch)
+		let yawRad = this.degreeToRadians(yaw)
+		
+		let sinYaw = Math.sin(yaw)
+		let sinPitch = Math.sin(pitch)
+		let cosYaw = Math.cos(yaw)
+		let cosPitch = Math.cos(pitch)
+		
+		return {
+			x : distance * -sinYaw * cosPitch ,
+			y : distance * -sinPitch ,
+			z : distance * cosYaw * cosPitch
+		};
+	}
+	
+	static toAngle (x, y, z) {
+		let magnitude = Math.sqrt(x*x + y*y + z*z)
+		
+		x /= magnitude
+		y /= magnitude
+		z /= magnitude
+		
+		let pitch = this.radianToDegrees(Math.asin(-y))
+		let yaw = this.radianToDegrees(Math.atan2(-x, z))
+		
+		return { pitch, yaw }
+	}
+}
