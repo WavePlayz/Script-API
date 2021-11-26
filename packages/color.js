7e7569a61ed22c34c99cfe8a971176d507426522
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2021-11-26 06:09:32 +0530

    Create color.js

---
+++ packages/color.js
@@ -0,0 +1,18 @@
+export default class Color {
+	static DARK_RED = "§4";
+	static RED = "§c";
+	static GOLD = "§6";
+	static YELLOW = "§e";
+	static DARK_GREEN = "§2";
+	static GREEN = "§a";
+	static AQUA = "§b";
+	static DARK_AQUA = "§3";
+	static DARK_BLUE = "§1";
+	static BLUE = "§9";
+	static LIGHT_PURPLE = "§d";
+	static DARK_PURPLE = "§5";
+	static WHITE = "§f";
+	static GRAY = "§7";
+	static DARK_GRAY = "§8";
+	static BLACK = "§0";
+} 
