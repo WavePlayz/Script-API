fad9cf3aac51e3785f8131e7a8c83a0161a32ee1
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2021-11-26 06:16:55 +0530

    Update color.js

--- packages/color.js
+++ packages/color.js
@@ -1,18 +1,18 @@
 export default class Color {
-	static DARK_RED = "§4";
-	static RED = "§c";
-	static GOLD = "§6";
-	static YELLOW = "§e";
-	static DARK_GREEN = "§2";
-	static GREEN = "§a";
-	static AQUA = "§b";
-	static DARK_AQUA = "§3";
-	static DARK_BLUE = "§1";
-	static BLUE = "§9";
-	static LIGHT_PURPLE = "§d";
-	static DARK_PURPLE = "§5";
-	static WHITE = "§f";
-	static GRAY = "§7";
-	static DARK_GRAY = "§8";
-	static BLACK = "§0";
+	static get DARK_RED() { return "§4"; }
+	static get RED() { return "§c"; }
+	static get GOLD() { return "§6"; }
+	static get YELLOW() { return "§e"; }
+	static get DARK_GREEN() { return "§2"; }
+	static get GREEN() { return "§a"; }
+	static get AQUA() { return "§b"; }
+	static get DARK_AQUA() { return "§3"; }
+	static get DARK_BLUE() { return "§1"; }
+	static get BLUE() { return "§9"; }
+	static get LIGHT_PURPLE() { return "§d"; }
+	static get DARK_PURPLE() { return "§5"; }
+	static get WHITE() { return "§f"; }
+	static get GRAY() { return "§7"; }
+	static get DARK_GRAY() { return "§8"; }
+	static get BLACK() { return "§0"; }
 } 
