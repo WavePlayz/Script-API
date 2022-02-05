52b830977ed8b69915a927be90470ecf5034b62a
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2022-02-06 01:38:32 +0530

    Create polyhash.js

---
+++ libraries/vector/hash/polyHash/polyhash.js
@@ -0,0 +1,18 @@
+export default class {
+	static get (string, { p: 53, m: 1e9 + 9}) {
+		let powerOfP = 1
+		let hash = 0
+		
+		let aCharCode = "a".charCodeAt()
+		
+		for (let i = 0; i < string.length; i++) {
+			let charCode = string[ i ].charCodeAt()
+			
+			hash = (hash + (charCode - aCharCode + 1) * powerOfP) % m
+			
+			powerOfP = (powerOfP * p) % m
+		}
+	
+		return hash
+	}
+}
