cd6fdf0f51978220d1c05b97241ac481a4168b37
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2022-03-14 03:18:14 +0530

    Update and rename libraries/vector/hash/polyHash/polyhash.js to libraries/hash/polyHash/polyhash.js

--- libraries/hash/polyHash/polyhash.js
+++ libraries/hash/polyHash/polyhash.js
@@ -1,9 +1,9 @@
 export default class {
-	static get (string, { p = 53, m = 1e9 + 9 } = {}) {
+	static get (string, { p = 153, m = 1e11 + 9 } = {}) {
 		let powerOfP = 1
 		let hash = 0
 		
-		let aCharCode = "a".charCodeAt()
+		let aCharCode = " ".charCodeAt()
 		
 		for (let i = 0; i < string.length; i++) {
 			let charCode = string[ i ].charCodeAt()
