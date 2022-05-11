06649bb2c87f87794ce1dd0af755aa3cef678202
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2022-05-11 10:00:18 +0530

    update form-builder.js

--- packages/form/formBuilder/0.1.0/form-builder.js
+++ packages/form/formBuilder/0.1.0/form-builder.js
@@ -17,37 +17,73 @@ function validateType (value, type, typeString) {
 		
 	if (isValid) return true;
 	
-	throw new Error(`invalid-type at ${value}, required ${typeString ?? type}`)
+	throw new Error(`invalid-type at ${value}, required ${ typeString ?? type.name }`)
 }
 
+
+class FormBuilderField {
+	constructor (formBuilder) {
+		this.formBuilder = formBuilder
+	}
+	
+	onFieldInput (callback) {
+		validateType(callback, Function)
+		
+		callback( inputHandler )
+		
+		return formBuilder
+	}
+}
 
 class FormBuilder {
 	static get VERSION() { return VERSION }
 	
-	static #getFormType (form) {
-		if ( form instanceof ActionFormData )
-			return ActionFormData;
-		
-		if ( form instanceof MessageFormData )
-			return MessageFormData;
-
-		if ( form instanceof ModalFormData )
-			return ModalFormData;
+	static VALID_ACTION_FIELDS = [ "button" ]
+	static VALID_MESSAGE_FIELDS = [ "button1", "button2" ]
+	static VALID_MODAL_FIELDS = [ "dropdown", "icon", "slider", "textField" "toggle" ]
+	
+	static VALID_FIELDS = [ this.VALID_ACTION_FIELDS, this.VALID_MESSAGE_FIELDS, this.VALID_MODAL_FIELDS ]
+	
+	static getFormFromFieldName (name) {
+		if ( VALID_ACTION_FIELDS.includes( name ) ) {
+			return ActionFormData
+		}
 	}
 	
-	
-	constructor () {
-		this.form = form
-		this.formType = this.constructor .#getFormType( form )
+	constructor (title) {
+		this.form = null
 		this.player = null
+		this.fields = new Map()
 	}
 	
-	setPlayer (player) {
-		this.player = player
+	#validateField (name) {
+		let includes = this.constructor
+			.VALID_FIELDS
+			.includes( name )
+			
+		if (includes) return true;
+		
+		throw new Error( `invalid-field at ${name}` )
+			
 	}
 	
-	addField (name, value) {
-		this.form[ name ][]
+	#setField (name, ...args) {
+		this.#validateField( name )
+		
+		if (this.form == null) {
+			this.form = this.constructor
+				.getFormFromFieldName(name)
+		}
+		
+		this.form[ name ](...args)
+	}
+	
+	add () {
+		const formBuilder = this
+		
+		return new FormBuilderField(this, inputHandler => {
+			formBuilder.#setField( ...arguments )
+		})
 	}
 	
 	
@@ -61,12 +97,28 @@ class FormBuilder {
 	}
 	
 	#onResponse (response) {
+		const { isCanceled } = response;
+		
+		if ( isCanceled ) {
+			this.#onCancel( response )
+		}
+		
 		
 	}
 }
 
 
 
+new FormBuilder("hello world")
+	.add("slider", 0 )
+	.onFieldInput( => {
 
-
+	})
+	
+	.add('button', "")
+	.onInput()
+	
+	.on("error", )
+	.on("can)
+	.show(player)
 
