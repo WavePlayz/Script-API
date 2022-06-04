3ef005126cf8e94b76056001346b4893c083e3f9
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2022-06-04 17:15:19 +0530

    update kramacaya.js

--- libraries/kramacaya/0.1.0/kramacaya.js
+++ libraries/kramacaya/0.1.0/kramacaya.js
@@ -3,11 +3,15 @@ const factorial = n => (n < 2)? (1) : (n * factorial(n-1))
 
 class Kramacaya {
 	static permutations (n, r, repeat = true) {
-		return repeat ? n**r
+		return (repeat) 
+			? (n**r) 
+			: (factorial(n) / factorial(n-r))
 	}
 	
-	static permutationsNoRepeat (n, r) {
-		return factorial(n) / factorial(n-r)
+	static combinations (n, r, repeat = false) {
+		return (repeat) 
+			? ( this.permutations(n,r,false) * (1 / factorial(r)) )
+			: factorial(r + n - 1)  /  (factorial(r) * factorial(n-1))
 	}
 	
 	
